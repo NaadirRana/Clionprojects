44db711c8d4ef03afd10a5696e1ec6652f5b1b70
Rana,Naadir Abdulaziz <naadir.rana@strathmore.edu>
2024-09-15 13:13:29 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std ;
+int main() {
+    int usd='500';
+    double rate='229.75';
+    int ksh=usd*rate;
+    cout<<ksh<<
+    system("pause");
+    return0:
+}
+
+
+
+
+
+
+
+
+
+
